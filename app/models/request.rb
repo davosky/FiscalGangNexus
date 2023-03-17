b3e2e3681497bb8f718ce6d0e9c13f896fba6a04
Davo Davosky <davodavosky@gmail.com>
2023-03-17 17:04:45 +0100

    Refined Again Requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -76,6 +76,7 @@ class Request < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :mobile, presence: true
+  validates :mobile, length: { maximum: 15 }
   validates :practice_id, presence: true
   validates :unprocessable_reason, presence: true, if: :unprocessable?
   validates :unprocessable, presence: true, if: :unprocessable_reason?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_12_132152) do
+ActiveRecord::Schema[7.0].define(version: 4) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.integer "position"
